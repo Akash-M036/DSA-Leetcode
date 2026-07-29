c3abf64875ca3977ab6204ae7e142e86fd65fdd5
Akash-M036 <akash.musidipilli@gmail.com>
2026-07-29 11:26:02 +0530

    Time: 1 ms (85.88%) | Memory: 44.7 MB (44.63%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    String[] pts = {"(", ")"};
+    List<String> ans = new ArrayList<>();
+    public void para(int n, StringBuilder sb, int cntl, int cntr) {
+        if (sb.length() == 2 * n) {
+            ans.add(sb.toString());
+            return;
+        }
+        for (int i = 0; i < 2; i++) {
+            if (i == 0 && cntl >= n) continue;
+            if (i == 1 && cntr >= cntl) continue;
+
+            int left = (i == 0) ? 1 : 0;
+            int right = (i == 1) ? 1 : 0;
+
+            sb.append(pts[i]);
+            para(n, sb, cntl + left, cntr + right);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        ans.clear();
+        para(n, new StringBuilder(), 0, 0);
+        return ans;
+    }
+}
