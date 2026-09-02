2bb9966e532ccc913bc654e6357e29e03176d2f2
Akash-M036 <akash.musidipilli@gmail.com>
2026-09-02 05:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (5.31%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,8 @@
+class Solution {
+    // odd - odd+odd = even
+    // even  - even + even =even
+    // odd +even = odd
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
