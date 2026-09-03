275b4fd37c5183e0b73924c6a1fa19c0881d8788
Akash-M036 <akash.musidipilli@gmail.com>
2026-09-03 07:46:17 +0530

    Time: 36 ms (5.07%) | Memory: 117.9 MB (99.28%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    /*
+        o-o =e  --> start odd will be always odd
+        e-o =o -->change to odd only
+        e-e =e -->not req
+    */
+    public boolean uniformArray(int[] nums1) {
+        int cntodd =0;
+        int cnteven =0;
+        for(int i =0; i< nums1.length;i++){
+            if(nums1[i]%2==0)
+                cnteven++;
+            else
+                cntodd++;
+        }
+        if(cnteven==0 || cntodd==0)
+            return true;
+        Arrays.sort(nums1);
+         cntodd =0;
+         cnteven =0;
+         boolean ans =true;
+         //odd
+        for(int i =0; i< nums1.length;i++){
+            if(nums1[i]%2!=1){
+                if(cntodd==0)
+                    ans = false;
+            }
+            if(nums1[i]%2==0)
+                cnteven++;
+            else
+                cntodd++;
+
+        }
+         return ans;
+    }
+}
